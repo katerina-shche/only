8b4f4feaf01df3e7f56d84e3c9c221aed8b36b89
katerina-shche <asti.katerina@gmail.com>
2023-09-08 19:39:32 +0200

    set up

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const container = document.getElementById('container');
 const map = document.getElementById('map');
 const overlay = document.getElementById('overlay');
-console.log(container, map, overlay);
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
 
-
-
+// addEventListener to size overlay on page load
+const onLoad = () => {
+    overlay.style.width = `${window.innerWidth}px`;
+    overlay.style.height = `${(window.innerWidth * 1080 / 1920)}px`; 
+    buttons[0].style.top = 
+    console.log(buttons[0]);
+}
 const onResize = () => {
     container.style.width = `${window.innerWidth}px`;
     container.style.height = `${(window.innerWidth * 1080 / 1920)}px`;
@@ -14,4 +20,5 @@ const onResize = () => {
     overlay.style.height = `${(window.innerWidth * 1080 / 1920)}px`;
     console.log(window.innerWidth);
 };
-window.addEventListener("resize", onResize)
+window.addEventListener("resize", onResize);
+window.addEventListener("load", onLoad);
