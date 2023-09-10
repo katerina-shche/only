0e62c5d9105254cb941530dcb3a38dab492608dd
katerina-shche <asti.katerina@gmail.com>
2023-09-10 17:23:08 +0200

    finished

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const bkgh = 1080;
 const bkgw = 1920;
+
 const container = document.getElementById('container');
 const map = document.getElementById('map');
 const overlay = document.getElementById('overlay');
@@ -7,15 +8,15 @@ const buttons = document.getElementsByTagName('button');
 
 const onLoad = () => {
     overlay.style.width = `${window.innerWidth}px`;
-    overlay.style.height = `${(window.innerWidth * 1080 / 1920)}px`; 
+    overlay.style.height = `${(window.innerWidth * bkgh / bkgw)}px`; 
 }
 const onResize = () => {
     container.style.width = `${window.innerWidth}px`;
-    container.style.height = `${(window.innerWidth * 1080 / 1920)}px`;
+    container.style.height = `${(window.innerWidth * bkgh / bkgw)}px`;
     map.style.width = `${window.innerWidth}px`;
-    map.style.height = `${(window.innerWidth * 1080 / 1920)}px`;
+    map.style.height = `${(window.innerWidth * bkgh / bkgw)}px`;
     overlay.style.width = `${window.innerWidth}px`;
-    overlay.style.height = `${(window.innerWidth * 1080 / 1920)}px`;
+    overlay.style.height = `${(window.innerWidth * bkgh / bkgw)}px`;
 };
 const onBtnClick = (e) => {
     if (!e.target.closest('.map-btn')) {
